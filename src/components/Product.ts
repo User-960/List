1690abb307d054c98c517ed5302e41b8547143e3
Ivan <ivanvolodin960@gmail.com>
2023-03-07 19:41:24 +0300

    feat: create class product

---
+++ src/components/Product.ts
@@ -0,0 +1,9 @@
+export default class Product {
+  readonly name: string;
+  readonly price: number;
+
+  constructor(name: string, price: number) {
+    this.name = name;
+    this.price = price;
+  }
+}
